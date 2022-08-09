fe32670ebe94567a2ed9e099b9baf71c3666e1d3
Harshada Lohar <Harshadal@emids.com>
2022-08-09 14:45:51 -0700

    [Harshada]Added: Unit test of AdminLogin Component,EmployeeLogin Component,Dashboard Component

--- EmployeeManagement/src/app/employeelogin/employeelogin.component.spec.ts
+++ EmployeeManagement/src/app/employeelogin/employeelogin.component.spec.ts
@@ -1,17 +1,25 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientModule } from '@angular/common/http';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { RouterTestingModule } from '@angular/router/testing';
 import { EmployeeloginComponent } from './employeelogin.component';
 
-describe('EmployeeloginComponent', () => {
+describe('Employeelogin', () => {
   let component: EmployeeloginComponent;
   let fixture: ComponentFixture<EmployeeloginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ EmployeeloginComponent ]
-    })
-    .compileComponents();
-  });
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [EmployeeloginComponent],
+      imports: [
+        ReactiveFormsModule,
+        RouterTestingModule,
+        HttpClientModule,
+        MatSnackBarModule
+      ],
+    }).compileComponents();
+  }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmployeeloginComponent);
@@ -19,7 +27,16 @@ describe('EmployeeloginComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create a Emp login component', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain component Employee Login', () => {
+    expect(component.loginForm).toBeTruthy();
+  });
+
+  it('should contain login form of default value', () => {
+    expect(component.loginForm.value).toEqual({ email: '', password: '' });
+  });
+
 });
