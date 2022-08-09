85ef9b433976bb32db87bc42bbb9ae1bf67466ba
Harshada Lohar <Harshadal@emids.com>
2022-08-08 18:27:27 -0700

    [Harshada]Added: Unit test of EmployeeService,AppComponent,HttpService,AdminService,HomeComponent

--- EmployeeManagement/src/app/Services/HttpService/http.service.spec.ts
+++ EmployeeManagement/src/app/Services/HttpService/http.service.spec.ts
@@ -1,16 +1,23 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './http.service';
 
 describe('HttpService', () => {
-  let service: HttpService;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(HttpService);
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [HttpService]
+  }));
 
   it('should be created', () => {
+    const service: HttpService = TestBed.get(HttpService);
     expect(service).toBeTruthy();
   });
+
+  it('should have getData function', () => {
+    const service: HttpService = TestBed.get(HttpService);
+    expect(service.getService).toBeTruthy();
+  });
+
 });
